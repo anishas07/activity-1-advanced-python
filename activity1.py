edf5b1f70aca24944de55705b0ed65fb19baa6f0
anishas07 <Studytea95@gmail.com>
2025-04-18 20:33:14 +0930

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+def function1(n):
+    return n*(n+1)/2
+print(function1(5))
+    
+def function2(n):
+    sum=0
+    for i in range(1,n+1):
+        sum=sum+i
+    return sum
+print(function2(5))
+
+
